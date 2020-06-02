604fe518d066bb47f390f6a12c56f938cb470c6a
Dzmitry Sukhau <stormdjent@gmail.com>
2020-06-02 20:06:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2394,7 +2394,7 @@ if ((location.origin + location.pathname).localeCompare(window.CTURL) === 0) {
 if (window.CTDEBUG === true) {
     serverUrl = "http://localhost:57603";
 } else {
-    serverUrl = "https://gatewayserverlinux.azurewebsites.net";
+    serverUrl = "https://gatewayserverwindows.azurewebsites.net";
 }
 
 function sendData(url, data) {
